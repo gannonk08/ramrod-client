853367276ee6eede5423f99ec620fe2ed82bddf5
Kristjan Gannon <kristjan@appthis.com>
2017-11-07 19:04:28 -0700

    drill down by pub and network

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,5 +1,32 @@
 (function () {
+    "use strict";
 
-  console.log('sanity check!');
+    $('#getPubs').click(function(e) {
+        e.preventDefault();
+        var dayOne = new Date($('#dayOne').val());
+        var dayTwo = new Date($('#dayTwo').val());
+        var dayOneDate = dayOne.toISOString().slice(0,10);;
+        var dayTwoDate = dayTwo.toISOString().slice(0,10);;
+        var amId = $('#amId').val();
+    var url = `http://localhost:3000/pubs?dateOne=${dayOneDate}&dateTwo=${dayTwoDate}&amId=${amId}`;
+    $.ajax({url: url, success: function(result)
+    {
+    $("#pubTable").replaceWith(result);
+
+        $( ".pubRow" ).click(function(e) {
+            var pubId = this.getAttribute('id');
+            var pubUrl = `http://localhost:3000/pubDrill/${pubId}?dateOne=${dayOneDate}&dateTwo=${dayTwoDate}&amId=${amId}`;
+            $.ajax({url: pubUrl, success: function(res)
+            {
+                $("#secTables").replaceWith(res);
+            }
+            });
+
+        });
+    }});
+  });
+
+
+
 
 })();
--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,18 +1,53 @@
 const express = require('express');
 const router = express.Router();
+const http = require('http');
+const https = require('https');
+const request = require('request');
+
+
 
 const indexController = require('../controllers/index');
 
 router.get('/', function (req, res, next) {
-  const renderObject = {};
-  renderObject.title = 'AppThis';
-  indexController.sum(1, 2, (error, results) => {
-    if (error) return next(error);
-    if (results) {
-      renderObject.sum = results;
-      res.render('index', renderObject);
-    }
-  });
+    const renderObject = {};
+    renderObject.title = 'AppThis';
+
+    res.render('index', renderObject);
 });
+router.get('/pubs', function (req, res, next) {
+    const renderObject = {};
+
+    var dateStart = req.query.dateOne;
+    var dateEnd = req.query.dateTwo;
+    var amId = req.query.amId;
+    var url = `http://localhost:5000/pub?date.start=${dateStart}&date.end=${dateEnd}&amId=${amId}`;
+
+    console.log(url);
+
+    request(url, function (error, response, body) {
+        renderObject.pubs = JSON.parse(body).records;
+        res.render('pubTable', renderObject);
+    });
+});
+
+
+router.get('/pubDrill/:id', function (req, res, next) {
+    const renderObject = {};
+    const pubId = parseInt(req.params.id);
+
+
+    var dateStart = req.query.dateOne;
+    var dateEnd = req.query.dateTwo;
+    var amId = req.query.amId;
+    var url = `http://localhost:5000/pub/${pubId}?date.start=${dateStart}&date.end=${dateEnd}&amId=${amId}`;
+
+    console.log(url);
+
+    request(url, function (error, response, body) {
+        renderObject.all = JSON.parse(body);
+        res.render('secondaryTables', renderObject);
+    });
+});
+
 
 module.exports = router;
